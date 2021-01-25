cef4e4e6bbb91ded547164d72d5bfdbbf9e2abdc
kiyoungkim1 <kky416@gmail.com>
2021-01-25 09:50:50 +0900

    TFBart compute_loss for id ne -100

--- src/transformers/models/bart/modeling_tf_bart.py
+++ src/transformers/models/bart/modeling_tf_bart.py
@@ -37,6 +37,7 @@ from ...modeling_tf_outputs import (
 
 # Public API
 from ...modeling_tf_utils import (
+    TFCausalLanguageModelingLoss,
     DUMMY_INPUTS,
     TFPreTrainedModel,
     TFSharedEmbeddings,
@@ -1336,15 +1337,3 @@ class TFBartForConditionalGeneration(TFBartPretrainedModel):
             return tf.where(vocab_range != self.config.eos_token_id, LARGE_NEGATIVE, logits)
         else:
             return logits
-
-    def compute_loss(self, labels, logits):
-        """CrossEntropyLoss that ignores pad tokens"""
-        loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(
-            from_logits=True,
-            reduction=tf.keras.losses.Reduction.NONE,
-        )
-        melted_labels = tf.reshape(labels, (-1,))
-        active_loss = tf.not_equal(melted_labels, self.config.pad_token_id)
-        reduced_logits = tf.boolean_mask(tf.reshape(logits, (-1, shape_list(logits)[2])), active_loss)
-        labels = tf.boolean_mask(melted_labels, active_loss)
-        return loss_fn(labels, reduced_logits)
